624157dbc4b2e2e6efb72bfdcefa086dfd5a6f2b
LW068 <ricardocoronaberron@gmail.com>
2024-03-21 15:50:22 -0500

    [Attempt #1] testing if workflow works.

--- website/wp-contents/themes/twentytwentyfour-child/functions.php
+++ website/wp-contents/themes/twentytwentyfour-child/functions.php
@@ -4,3 +4,4 @@ function my_child_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
 }
+## testing if it works!! 1.0
