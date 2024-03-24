3fc2d95964390fce7069aad0c1131068536f2130
LW068 <ricardocoronaberron@gmail.com>
2024-03-24 16:29:39 -0500

    [Fixed]: staging setup #5

--- website/public_html/wp-contents/themes/twentytwentyfour-child/functions.php
+++ website/public_html/wp-contents/themes/twentytwentyfour-child/functions.php
@@ -4,4 +4,4 @@ function my_child_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
 }
-## # on staging branch - test #4
+## # on staging branch - test #5
