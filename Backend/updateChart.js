9331c1ab16c2e462518d8e6ccd0816133d5a2aa9
mwblevins <79066736+mwblevins@users.noreply.github.com>
2024-04-02 16:44:30 -0500

    Working with no errors, but is opening a new window each time since the call is making a new html file

--- Backend/updateChart.js
+++ Backend/updateChart.js
@@ -13,16 +13,10 @@ function updateChart() {
     });
 }
 
-// Function to refresh the page
-function refreshPage() {
-    window.location.reload();
-}
-
 // Update the chart immediately on script start
 updateChart();
 
 // Refresh the page every 60 seconds (60000 milliseconds)
 setInterval(() => {
     updateChart(); // Update the chart data
-    refreshPage(); // Refresh the page
 }, 60000); // 60 seconds interval
